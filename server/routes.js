5ecb8111d5adae407e8098fe3d534483ae921ba4
Theo Reyes <theo.reyes@simplyrfid.com>
2020-09-29 01:59:23 +0800

    Added cors middleware

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const teamApi = require('./api/teams');
 const commandApi = require('./api/commands');
 
 module.exports = app => {
   app.use(express.json());
+  app.use(cors());
   app.use('/api/teams', teamApi);
   app.use('/api/commands', commandApi);
 }
